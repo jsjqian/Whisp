35cbd814166716c1f683311c0dccb779a6dad15a
AyeAndy <msoohyun88@gmail.com>
2016-01-30 05:46:52 -0500

    adlkhlkasjldf

--- app/src/main/java/com/example/jack/whisp/MainActivity.java
+++ app/src/main/java/com/example/jack/whisp/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String[] dummy = {"wheat", "rye", "sourdough"};
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,24 @@ public class MainActivity extends AppCompatActivity {
         ListView list = (ListView)findViewById(R.id.list);
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, dummy);
         list.setAdapter(adapter);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
 
 }
