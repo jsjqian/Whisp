9ee44a6868389505f5f4328451f10738ad1262b9
AyeAndy <msoohyun88@gmail.com>
2016-01-30 10:18:25 -0500

    oh

--- app/src/main/java/com/example/jack/whisp/MainActivity.java
+++ app/src/main/java/com/example/jack/whisp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.jack.whisp;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
 //IMPORTS FOR THE AUDIO CAPTURE//
 
@@ -17,9 +23,11 @@ import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 //END OF IMPORTS FOR AUDIO CAPTURE//
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener {
 
 
     //VARS FOR AUDIO CAPTURE
@@ -35,14 +43,18 @@ public class MainActivity extends AppCompatActivity {
     //END VARS FOR AUDIO CAPTURE
 
     private static final String[] dummy = {"wheat", "rye", "sourdough"};
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView list = (ListView)findViewById(R.id.list);
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item, dummy);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.row, R.id.text11, dummy);
         list.setAdapter(adapter);
+
         b1=(Button)findViewById(R.id.button);
         b1.setOnTouchListener(new View.OnTouchListener() {
 
@@ -62,6 +74,15 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
@@ -117,4 +138,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            ((TextView)findViewById(R.id.longLat)).setText(String.valueOf(mLastLocation.getLatitude()));
+            ((TextView)findViewById(R.id.longLat)).setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
