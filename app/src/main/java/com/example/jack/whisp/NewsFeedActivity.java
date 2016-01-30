a86f7ace088940f7ffb36023886c8bd0e6657ba8
jpan1997 <j.pan1997@gmail.com>
2016-01-30 05:29:49 -0500

    Splash SCREEN

---
+++ app/src/main/java/com/example/jack/whisp/NewsFeedActivity.java
@@ -0,0 +1,13 @@
+package com.example.jack.whisp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewsFeedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+    }
+}
